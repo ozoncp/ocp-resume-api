3fd687608703220fac8e020d5b9613050a6a4519
NewDenis <pimenov.d.v@yandex.ru>
2021-06-02 17:33:29 +0300

    added empty tests

--- internal/repo/repo_test.go
+++ internal/repo/repo_test.go
@@ -4,9 +4,17 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
+	"github.com/ozoncp/ocp-resume-api/internal/achievement"
 	"github.com/ozoncp/ocp-resume-api/internal/repo"
+	"github.com/ozoncp/ocp-resume-api/internal/resume"
 )
 
 var _ = Describe("Repo", func() {
-
+	r := repo.NewRepo(11)
+	achsA := make([]achievement.Achievement, 10)
+	achsR := make([]resume.Resume, 10)
+	err := r.AddAchievements(achsA)
+	Expect(err).ShouldNot(BeNil())
+	err = r.AddResumes(achsR)
+	Expect(err).ShouldNot(BeNil())
 })
